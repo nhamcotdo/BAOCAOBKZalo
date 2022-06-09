c4119e770d71deba4edfa415783d0251033b3114
nhamcotdo <nhamqs2002@gmail.com>
2022-06-09 14:13:06 +0700

    Initial commit

---
+++ BKZALO/src/Main/MulticastSender.java
@@ -0,0 +1,35 @@
+package Main;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public class MulticastSender {
+
+	public static final String GROUP_ADDRESS = "239.255.255.252";
+	public static final int PORT = 8888;
+	public static InetAddress address ;
+	public static void Send(String msg)
+	{
+		DatagramSocket socket = null;
+		try {
+			// Get the address that we are going to connect to.
+			address = InetAddress.getByName(GROUP_ADDRESS);
+
+			// Create a new Multicast socket
+			socket = new DatagramSocket();
+
+			DatagramPacket outPacket = null;
+				outPacket = new DatagramPacket(msg.getBytes(), msg.getBytes().length, address, PORT);
+				socket.send(outPacket);
+			
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		} finally {
+			if (socket != null) {
+				socket.close();
+			}
+		}
+	}
+}
